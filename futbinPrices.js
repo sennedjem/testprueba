409c7b5dba20dd80b6e2ba8dcd53d8ceca8e0f68
sennedjem <sebastianariell@gmail.com>
2022-11-10 00:18:07 -0300

    Update futbinPrices.js

--- futbinPrices.js
+++ futbinPrices.js
@@ -80,6 +80,96 @@ UTPaginatedItemListView.prototype.renderItems = function(e) {
 }
 
 
+window.mostrarPrecioFutbinEnVista = function(price, cell, context, parameter_function = null){
+  if(parameter_function){
+    var filterValue = parameter_function()
+  } else {
+    var filterValue = true
+  }  
+  if(price){  
+      console.log(cell)
+      var elem = document.getElementById("#futbinPrice-"+cell.data.id);
+      if(elem){
+          elem.parentElement.removeChild(elem);
+      }
+
+      cell.data.futbinPrice = price
+      newDiv = document.createElement( "div" );
+      newDiv.classList.add('futbinPrice')
+      newDiv.id = 'futbinPrice-' + cell.data.id
+      newDiv.innerHTML = 'Futbin price: ' + cell.data.futbinPrice + ' (' + window.percentage(parseInt(cell.data.futbinPrice.replace(/,/g, ''), 10),95) + ' tax)'
+      newDiv.style.color = 'coral'
+
+      newDivGrid = document.createElement( "div" );
+      newDivGrid.classList.add('futbinPriceGrid')
+      newDivGrid.id = 'futbinPriceGrid-' + cell.data.id
+      newDivGrid.style.display = 'none'
+      newDivGrid.style['text-align'] = 'text-align: center'
+      newDivGrid.style['background-color'] = '#ffa50082'
+
+      priceGridDiv = document.createElement('div');
+      priceGridDiv.classList.add('money')
+      priceGridDiv.style.color = 'white'
+      priceGridDiv.innerHTML = '' + cell.data.futbinPrice 
+
+      newDivGrid.appendChild(priceGridDiv)
+
+
+      context.minInput = new UTNumericInputSpinnerControl,
+      cell.__entityContainer.childNodes[2].after(context.minInput.getRootElement())
+      context.minInput.getRootElement().style.margin='5px'
+      context.minInput.init()
+      context.minInput.getRootElement().style.display = 'none'
+      context.minInput.setValue(parseInt(cell.data.futbinPrice.replace(/,/g, ''), 10))
+      context.minInput._currencyInput.decrease()
+      context.maxInput = new UTNumericInputSpinnerControl,
+      cell.__entityContainer.childNodes[2].after(context.maxInput.getRootElement())
+      context.maxInput.getRootElement().style.margin='5px'
+      context.maxInput.init()
+      context.maxInput.getRootElement().style.display = 'none'
+      context.maxInput.setValue(parseInt(cell.data.futbinPrice.replace(/,/g, ''), 10))
+      cell.data.futbinBid = context.minInput.getValue()
+      cell.data.futbinBuyNow = context.maxInput.getValue()
+      if(window.onMarket()){
+        cell.data.profit = window.percentage(parseInt(cell.data.futbinPrice.replace(/,/g, ''), 10),95) - cell.data._auction.buyNowPrice,95
+        cell.data.profitPercentage = (window.percentage(parseInt(cell.data.futbinPrice.replace(/,/g, ''), 10),95)  / (cell.data._auction.buyNowPrice/100)) - 100 
+        profitDiv = document.createElement( "div" );
+        profitDiv.classList.add('futbinPrice')
+        profitDiv.innerHTML = 'Profit: ' + cell.data.profit.toString() + ' (' + cell.data.profitPercentage.toFixed(2) + '%)'
+        if(cell.data.profit>0){
+          profitDiv.style.color = '#1bff00'
+          cell.__root.style['background-color'] = '#064726'
+        } 
+        if(!$('#futbinPrice-'+cell.data.id).length && cell.data.profit>0){
+          cell.__entityContainer.childNodes[2].after(profitDiv)
+        }
+      } else { 
+        if(cell.data.lastSalePrice && filterValue){
+            cell.data.profit = window.percentage(parseInt(cell.data.futbinPrice.replace(/,/g, ''), 10),95) - cell.data.lastSalePrice
+            cell.data.profitPercentage = (window.percentage(parseInt(cell.data.futbinPrice.replace(/,/g, ''), 10),95)  / (cell.data.lastSalePrice/100)) - 100 
+            profitDiv = document.createElement( "div" );
+            profitDiv.classList.add('futbinPrice')
+            profitDiv.innerHTML = 'Profit: ' + cell.data.profit.toString() + ' (' + cell.data.profitPercentage.toFixed(2) + '%)'
+            if(cell.data.profit>0){
+              profitDiv.style.color = '#1bff00'
+              cell.__root.style['background-color'] = '#064726'
+            } else {
+              cell.__root.style['background-color'] = '#470608'
+              profitDiv.style.color = '#ff0000' 
+            }
+            if(!$('#futbinPrice-'+cell.data.id).length){
+
+              cell.__entityContainer.childNodes[2].after(profitDiv)
+            }
+        }
+      }
+      if(!$('#futbinPrice-'+cell.data.id).length){
+        cell.__root.appendChild(newDivGrid)
+        cell.__entityContainer.childNodes[2].after(newDiv)
+      }
+  }
+}
+
 //backend.js
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   // 2. A page requested user data, respond with a copy of `user`
